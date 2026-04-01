9c5cca426a72bd6c566107649ce5c8b514972020
Nikhil Soni <nikhil.soni@signoz.io>
2026-04-01 11:49:24 +0530

    feat: setup types and interface for waterfall v3
    v3 is required for udpating the response json of
    the waterfall api. There wont' be any logical change.
    Using this requirement as an opportunity to move
    waterfall api to provider codebase architecture from
    older query-service

--- pkg/types/tracedetailtypes/waterfall.go
+++ pkg/types/tracedetailtypes/waterfall.go
@@ -10,9 +10,10 @@ import (
 
 // WaterfallRequest is the request body for the v3 waterfall API.
 type WaterfallRequest struct {
-	SelectedSpanID   string   `json:"selectedSpanId"`
-	UncollapsedSpans []string `json:"uncollapsedSpans"`
-	Limit            uint     `json:"limit"`
+	SelectedSpanID              string   `json:"selectedSpanId"`
+	IsSelectedSpanIDUnCollapsed bool     `json:"isSelectedSpanIDUnCollapsed"`
+	UncollapsedSpans            []string `json:"uncollapsedSpans"`
+	Limit                       uint     `json:"limit"`
 }
 
 // WaterfallResponse is the response for the v3 waterfall API.
@@ -25,25 +26,22 @@ type WaterfallResponse struct {
 	TotalSpansCount               uint64            `json:"totalSpansCount"`
 	TotalErrorSpansCount          uint64            `json:"totalErrorSpansCount"`
 	ServiceNameToTotalDurationMap map[string]uint64 `json:"serviceNameToTotalDurationMap"`
-	Spans                         []*WaterfallSpan  `json:"spans"`
+	Spans                         []*Span           `json:"spans"`
 	HasMissingSpans               bool              `json:"hasMissingSpans"`
 	UncollapsedSpans              []string          `json:"uncollapsedSpans"`
 }
 
 // Event represents a span event.
 type Event struct {
-	Name         string         `json:"name,omitempty"`
-	TimeUnixNano uint64         `json:"timeUnixNano,omitempty"`
-	AttributeMap map[string]any `json:"attributeMap,omitempty"`
-	IsError      bool           `json:"isError,omitempty"`
+	Name         string                 `json:"name,omitempty"`
+	TimeUnixNano uint64                 `json:"timeUnixNano,omitempty"`
+	AttributeMap map[string]interface{} `json:"attributeMap,omitempty"`
+	IsError      bool                   `json:"isError,omitempty"`
 }
 
-// WaterfallSpan represents the span in waterfall response,
-// this uses snake_case keys for response as a special case since these
-// keys can be directly used to query spans and client need to know the actual fields.
-// This pattern should not be copied elsewhere.
-type WaterfallSpan struct {
-	Attributes         map[string]any    `json:"attributes"`
+// Span is the v3 span format with separated attributes/resources and snake_case keys.
+type Span struct {
+	Attributes         map[string]string `json:"attributes"`
 	DBName             string            `json:"db_name"`
 	DBOperation        string            `json:"db_operation"`
 	DurationNano       uint64            `json:"duration_nano"`
@@ -61,7 +59,7 @@ type WaterfallSpan struct {
 	Links              string            `json:"links"`
 	Name               string            `json:"name"`
 	ParentSpanID       string            `json:"parent_span_id"`
-	Resource           map[string]string `json:"resource"`
+	Resources          map[string]string `json:"resources"`
 	ResponseStatusCode string            `json:"response_status_code"`
 	SpanID             string            `json:"span_id"`
 	StatusCode         int32             `json:"status_code"`
@@ -70,12 +68,12 @@ type WaterfallSpan struct {
 	Timestamp          string            `json:"timestamp"`
 	TraceID            string            `json:"trace_id"`
 	TraceState         string            `json:"trace_state"`
-
 	// Tree structure fields
-	Children         []*WaterfallSpan `json:"-"`
-	SubTreeNodeCount uint64           `json:"sub_tree_node_count"`
-	HasChildren      bool             `json:"has_children"`
-	Level            uint64           `json:"level"`
+	Children         []*Span `json:"children"`
+	SubTreeNodeCount uint64  `json:"sub_tree_node_count"`
+	HasChildren      bool    `json:"has_children"`
+	HasSiblings      bool    `json:"has_siblings"`
+	Level            uint64  `json:"level"`
 
 	// timeUnixNano is an internal field used for tree building and sorting.
 	// It is not serialized in the JSON response.
@@ -84,18 +82,19 @@ type WaterfallSpan struct {
 	ServiceName string `json:"-"`
 }
 
-// CopyWithoutChildren creates a shallow copy and reset computed tree fields.
-func (s *WaterfallSpan) CopyWithoutChildren(level uint64) *WaterfallSpan {
+// CopyWithoutChildren creates a shallow copy and reset computed tree fields
+func (s *Span) CopyWithoutChildren(level uint64, hasSiblings bool) *Span {
 	cp := *s
 	cp.Level = level
 	cp.HasChildren = len(s.Children) > 0
-	cp.Children = make([]*WaterfallSpan, 0)
+	cp.HasSiblings = hasSiblings
+	cp.Children = make([]*Span, 0)
 	cp.SubTreeNodeCount = 0
 	return &cp
 }
 
-// SpanModel is the ClickHouse scan struct for the v3 waterfall query.
-type SpanModel struct {
+// SpanItem is the ClickHouse scan struct for the v3 waterfall query.
+type SpanItem struct {
 	TimeUnixNano       time.Time          `ch:"timestamp"`
 	DurationNano       uint64             `ch:"duration_nano"`
 	SpanID             string             `ch:"span_id"`
@@ -128,66 +127,6 @@ type SpanModel struct {
 	ResponseStatusCode string             `ch:"response_status_code"`
 }
 
-// ToSpan converts a SpanModel (ClickHouse scan result) into a Span for the waterfall response.
-func (item *SpanModel) ToSpan() *WaterfallSpan {
-	// Merge attributes_string, attributes_number, attributes_bool preserving native types
-	attributes := make(map[string]any, len(item.AttributesString)+len(item.AttributesNumber)+len(item.AttributesBool))
-	for k, v := range item.AttributesString {
-		attributes[k] = v
-	}
-	for k, v := range item.AttributesNumber {
-		attributes[k] = v
-	}
-	for k, v := range item.AttributesBool {
-		attributes[k] = v
-	}
-
-	resources := make(map[string]string)
-	maps.Copy(resources, item.ResourcesString)
-
-	events := make([]Event, 0, len(item.Events))
-	for _, eventStr := range item.Events {
-		var event Event
-		if err := json.Unmarshal([]byte(eventStr), &event); err != nil {
-			continue
-		}
-		events = append(events, event)
-	}
-
-	return &WaterfallSpan{
-		Attributes:         attributes,
-		DBName:             item.DBName,
-		DBOperation:        item.DBOperation,
-		DurationNano:       item.DurationNano,
-		Events:             events,
-		ExternalHTTPMethod: item.ExternalHTTPMethod,
-		ExternalHTTPURL:    item.ExternalHTTPURL,
-		Flags:              item.Flags,
-		HasError:           item.HasError,
-		HTTPHost:           item.HTTPHost,
-		HTTPMethod:         item.HTTPMethod,
-		HTTPURL:            item.HTTPURL,
-		IsRemote:           item.IsRemote,
-		Kind:               int32(item.Kind),
-		KindString:         item.SpanKind,
-		Links:              item.References,
-		Name:               item.Name,
-		ParentSpanID:       item.ParentSpanID,
-		Resource:           resources,
-		ResponseStatusCode: item.ResponseStatusCode,
-		SpanID:             item.SpanID,
-		StatusCode:         item.StatusCode,
-		StatusCodeString:   item.StatusCodeString,
-		StatusMessage:      item.StatusMessage,
-		Timestamp:          item.TimeUnixNano.Format(time.RFC3339Nano),
-		TraceID:            item.TraceID,
-		TraceState:         item.TraceState,
-		Children:           make([]*WaterfallSpan, 0),
-		TimeUnixNano:       uint64(item.TimeUnixNano.UnixNano()),
-		ServiceName:        item.ServiceName,
-	}
-}
-
 // TraceSummary is the ClickHouse scan struct for the trace_summary query.
 type TraceSummary struct {
 	TraceID  string    `ch:"trace_id"`
@@ -205,25 +144,25 @@ type OtelSpanRef struct {
 
 // WaterfallCache holds pre-processed trace data for caching.
 type WaterfallCache struct {
-	StartTime                     uint64                    `json:"startTime"`
-	EndTime                       uint64                    `json:"endTime"`
-	DurationNano                  uint64                    `json:"durationNano"`
-	TotalSpans                    uint64                    `json:"totalSpans"`
-	TotalErrorSpans               uint64                    `json:"totalErrorSpans"`
-	ServiceNameToTotalDurationMap map[string]uint64         `json:"serviceNameToTotalDurationMap"`
-	SpanIDToSpanNodeMap           map[string]*WaterfallSpan `json:"spanIdToSpanNodeMap"`
-	TraceRoots                    []*WaterfallSpan          `json:"traceRoots"`
-	HasMissingSpans               bool                      `json:"hasMissingSpans"`
+	StartTime                     uint64            `json:"startTime"`
+	EndTime                       uint64            `json:"endTime"`
+	DurationNano                  uint64            `json:"durationNano"`
+	TotalSpans                    uint64            `json:"totalSpans"`
+	TotalErrorSpans               uint64            `json:"totalErrorSpans"`
+	ServiceNameToTotalDurationMap map[string]uint64 `json:"serviceNameToTotalDurationMap"`
+	SpanIDToSpanNodeMap           map[string]*Span  `json:"spanIdToSpanNodeMap"`
+	TraceRoots                    []*Span           `json:"traceRoots"`
+	HasMissingSpans               bool              `json:"hasMissingSpans"`
 }
 
 func (c *WaterfallCache) Clone() cachetypes.Cacheable {
 	copyOfServiceNameToTotalDurationMap := make(map[string]uint64)
 	maps.Copy(copyOfServiceNameToTotalDurationMap, c.ServiceNameToTotalDurationMap)
 
-	copyOfSpanIDToSpanNodeMap := make(map[string]*WaterfallSpan)
+	copyOfSpanIDToSpanNodeMap := make(map[string]*Span)
 	maps.Copy(copyOfSpanIDToSpanNodeMap, c.SpanIDToSpanNodeMap)
 
-	copyOfTraceRoots := make([]*WaterfallSpan, len(c.TraceRoots))
+	copyOfTraceRoots := make([]*Span, len(c.TraceRoots))
 	copy(copyOfTraceRoots, c.TraceRoots)
 	return &WaterfallCache{
 		StartTime:                     c.StartTime,
